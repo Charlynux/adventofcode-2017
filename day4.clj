1252792cadb3b82dc950276633dafb820fd26267
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2017-12-04 19:06:04 +0100

    Day4 - Part 2

--- day4.clj
+++ day4.clj
@@ -16,3 +16,23 @@
         count))
 
 (count-valid data)
+
+(defn string-to-frequencies [text]
+        (frequencies (clojure.string/split text #"")))
+
+(defn is-valid-2 [text]
+    (->>
+       (clojure.string/split text #" ")
+       (map string-to-frequencies)
+       ((juxt identity set))
+       (map count)
+       (apply =)
+    )
+)
+
+(defn count-valid-2 [lines]
+    (->> 
+        (filter is-valid-2 lines)
+        count))
+
+(count-valid-2 data)
